f17ab07ec530db3c947910bb78235ac380d91557
CyberGA <etomu.joshua@gmail.com>
2023-08-19 15:04:27 -0700

    checks for null

--- _printf.c
+++ _printf.c
@@ -43,6 +43,9 @@ int _printf(const char *format, ...)
 	int i = 0;
 	va_list args;
 
+	if (!format || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+
 	va_start(args, format);
 	while (*format)
 	{
